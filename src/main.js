f8957f8a807843bffac451c6a3ec2c5786d4b7ce
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2025-11-22 14:50:45 +0100

    loop

--- src/main.js
+++ src/main.js
@@ -15,3 +15,21 @@ class Person {
   button.addEventListener("click", () => {
     text.innerHTML = erika.beskriv();
   });
+
+  const personer = [
+    new Person("Erika", 35, "SFI-lärare"),
+    new Person("Anna", 28, "Designer"),
+    new Person("Johan", 42, "Ingenjör"),
+    new Person("Sara", 31, "Frontend Developer"),
+    new Person("Karl", 50, "Lärare")
+  ];
+
+  const button2 = document.getElementById("show2");
+  const text2 = document.getElementById("text2");
+
+  button2.addEventListener("click", () => {
+    text2.innerHTML = "";
+  for (let i = 0; i < personer.length; i++) {
+    text2.innerHTML += personer[i].beskriv() + "<br>";
+  }
+});
