b82d5dad8d9de4f851103fbce3b4fad327310933
Erika Hörling <erikahorling@mbpsomtorerika2.home>
2025-11-22 14:39:35 +0100

    klass-funktion

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-const person = {
-  namn: "Erika",
-  alder: 35,
-  yrke: "Frontend Developer",
-  hobbyer: ["sjunga", "skriva", "spela fiol"]
+class Person {
+  constructor(namn, alder, yrke) {
+    this.namn = namn;
+    this.alder = alder;
+    this.yrke = yrke;
+    this.beskriv = function() {
+      return `${this.namn} är ${this.alder} år gammal.`;
+    };
+  }
 }
+  const erika = new Person("Erika", 35, "sfi-lärare");
+  const button = document.getElementById("show");
+  const text = document.getElementById("text");
+
+  button.addEventListener("click", () => {
+    text.innerHTML = erika.beskriv();
+  });
